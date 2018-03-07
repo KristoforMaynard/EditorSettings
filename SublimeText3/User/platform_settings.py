2fbd695b645038bb686d3d4818af7f927056aa3c
Kristofor Maynard <kristofor.maynard@gmail.com>
2018-03-07 17:07:16 -0500

    initial commit

---
+++ SublimeText3/User/platform_settings.py
@@ -0,0 +1,41 @@
+# https://github.com/quarnster/PlatformSettings/blob/master/platform_settings.py
+import sublime
+import sublime_plugin
+
+class PlatformSettingsEventListener(sublime_plugin.EventListener):
+    def check_settings(self, view, first=False):
+        s = view.settings()
+        default_keys = ["${platform}", "user_${platform}"]
+        keys = s.get("platform_settings_keys", default_keys)
+        if not keys:
+            keys = default_keys
+
+        if not first:
+            first = not s.get("platform_settings_was_here", False)
+        if not first:
+            s.clear_on_change("platform_settings")
+
+        platform_settings = {}
+        for key in keys:
+            key = key.replace("${platform}", sublime.platform())
+            platform_settings.update(s.get(key, {}) or {})
+
+        for key in platform_settings:
+            current = s.get(key, None)
+            value = platform_settings.get(key)
+            if current != value:
+                s.set(key, value)
+
+        def on_change():
+            self.check_settings(view)
+        s.set("platform_settings_was_here", True)
+        s.add_on_change("platform_settings", lambda: sublime.set_timeout(on_change, 0))
+
+    def on_activated(self, view):
+        self.check_settings(view)
+
+    def on_new(self, view):
+        self.check_settings(view, True)
+
+    def on_load(self, view):
+        self.check_settings(view, True)
